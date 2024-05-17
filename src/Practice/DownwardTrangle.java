deb44d5abc0a6f156ca7df3a91fbb828e2570f46
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-05-17 13:01:16 +0530

    Star practice

--- src/Practice/DownwardTrangle.java
+++ src/Practice/DownwardTrangle.java
@@ -1,4 +1,20 @@
 package Practice;
 
 public class DownwardTrangle {
+    public static void main(String[] args) {
+        drawStar();
+    }
+
+    public static void drawStar() {
+        int rows = 4;
+        for (int i = 0; i < rows; i++) {
+            for (int j = rows-i; j >=1; j--) {
+                System.out.print("*");
+            }
+            for (int k = 0; k <=i; k++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 }
