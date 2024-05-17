964dde66aab80166a10c8bfaf7ec911a72e50895
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-05-17 12:21:35 +0530

    Star practice

--- src/Practice/SpaceFirst.java
+++ src/Practice/SpaceFirst.java
@@ -1,4 +1,23 @@
 package Practice;
 
 public class SpaceFirst {
+    public static void main(String[] args) {
+        drawStar();
+    }
+
+    public static void drawStar() {
+        int rows = 4;
+        for (int i = 0; i < rows; i++) {
+
+            for (int k = rows - 1; k > i; k--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 }
