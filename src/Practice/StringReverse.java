557afd61dff75dd7a6b8bff355fb7718e44f944f
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-06-03 12:03:30 +0530

    String Practice

--- src/Practice/StringReverse.java
+++ src/Practice/StringReverse.java
@@ -3,20 +3,18 @@ package Practice;
 public class StringReverse {
     public static void main(String[] args) {
         String code = "Rishie";
-        String reversedCode = reverse(code);
-        System.out.println("Reversed string: " + reversedCode);
+        String reversedCode= reverse(code);
+        System.out.println(reversedCode);
+
     }
 
     static String reverse(String code) {
-        // Use a StringBuilder for easier string manipulation
-        StringBuilder reversed = new StringBuilder();
+        StringBuilder sb= new StringBuilder();
+        for(int i=code.length()-1; i>=0;i--){
+            sb.append(code.charAt(i));
 
-        // Iterate over the string from end to start and append each character to the StringBuilder
-        for (int i = code.length() - 1; i >= 0; i--) {
-            reversed.append(code.charAt(i));
         }
-
-        // Convert the StringBuilder back to a String
-        return reversed.toString();
+        return sb.toString();
     }
+
 }
