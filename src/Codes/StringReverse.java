fca722c51c09cae446bf22bf353ee298e4a45b9e
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-06-13 12:21:06 +0530

    Arr Practice

--- src/Codes/StringReverse.java
+++ src/Codes/StringReverse.java
@@ -14,11 +14,25 @@ public class StringReverse {
 //        return reverse.toString();
 //    }
 
-        static String stringReverse(String st) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = st.length() - 1; i >= 0; i--) {
-                sb.append(st.charAt(i));
-            }
-            return sb.toString();
+//        static String stringReverse(String st) {
+//            StringBuilder sb = new StringBuilder();
+//            for (int i = st.length() - 1; i >= 0; i--) {
+//                sb.append(st.charAt(i));
+//            }
+//            return sb.toString();
+//        }
+
+    static String stringReverse(String str) {
+        char[] charArray = str.toCharArray();
+        int left = 0, right = charArray.length - 1;
+        while (left < right) {
+            // Swap characters
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            left++;
+            right--;
         }
+        return new String(charArray);
+    }
 }
