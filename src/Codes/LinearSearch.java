2c815f6ac8288ed633eb573365b237a60158abcb
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-06-17 20:14:00 +0530

    Arr Practice

--- src/Codes/LinearSearch.java
+++ src/Codes/LinearSearch.java
@@ -1,4 +1,28 @@
 package Codes;
 
+import java.util.Scanner;
+
 public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 5, 7, 6, 9, 5, 3, 4};
+        System.out.print("Enter num to Search: ");
+        Scanner sn = new Scanner(System.in);
+        int num = sn.nextInt();
+        int result = linearSearch(arr,num);
+        if(result !=-1){
+            System.out.println(num + " Found in array");
+        }else {
+            System.out.println("num not found");
+        }
+
+    }
+
+    public static int linearSearch(int[] arr, int num) {
+        for (int j : arr) {
+            if (j == num) {
+                return j;
+            }
+        }
+        return -1;
+    }
 }
