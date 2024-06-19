8e5ce90db97ffb8846fccfd8b042570abde2361f
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-06-19 17:11:20 +0530

    Arr Practice

---
+++ src/Arrays/MaxinArray.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class MaxinArray {
+    public static void main(String[] args) {
+        int [] arr = new int []{1,5,7,3,4,5,9,4};
+        findMax(arr);
+
+    }
+    public static void findMax(int []arr){
+        int max=0;
+        for (int i :arr){
+            if(i>max){
+                max=i;
+            }
+        }
+        System.out.print("Largest in array: " + max);
+    }
+}
