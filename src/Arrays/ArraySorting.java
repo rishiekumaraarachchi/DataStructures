0b43c63b05aa3c77c025f3221d2f28232cfbc647
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-07-04 11:38:34 +0530

    Array Practices

--- src/Arrays/ArraySorting.java
+++ src/Arrays/ArraySorting.java
@@ -18,16 +18,13 @@ public class ArraySorting {
 
     public static void optimizedBubbleSort(int[] arr) {
         int n = arr.length;
-        boolean swapped;
         for (int i = 0; i < n - 1; i++) {
-            swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // Swap arr[j] and arr[j + 1]
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    swapped = true;
                 }
             }
             // If no elements were swapped, break the loop
