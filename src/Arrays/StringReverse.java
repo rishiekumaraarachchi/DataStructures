9976d44fb2b8b642e92680c82599ed0cfff37a1f
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-07-04 13:38:55 +0530

    Array Practices

---
+++ src/Arrays/StringReverse.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        String name= "Hello";
+        String reverse = stringReverse(name);
+        System.out.println(reverse );
+
+    }
+    public static String stringReverse(String x){
+        char [] charArray = x.toCharArray();
+        int left=0;
+        int right = x.length()-1;
+        while (left<right){
+            char temp=charArray[left];
+            charArray[left]=charArray[right];
+            charArray[right]=temp;
+            left++;
+            right--;
+        }
+        return new String(charArray);
+    }
+}
