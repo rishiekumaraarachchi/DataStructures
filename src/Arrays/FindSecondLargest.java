b08535947c3e700dcbc024d1314b970734ca56f8
rishie <rishieruween199611@gmail.com>
2024-07-18 07:41:14 +0530

    Arr practice

---
+++ src/Arrays/FindSecondLargest.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class FindSecondLargest {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6};
+        System.out.println("The second largest is: "+findSecondLargest(arr));
+    }
+    public static int findSecondLargest(int[] arr){
+        int max = arr[0];
+        int secondMax= arr[0];
+        for(int x: arr){
+            if(x>max){
+                secondMax=max;
+                max=x;
+            }
+        }
+        return secondMax;
+    }
+}
