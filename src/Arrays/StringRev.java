934774ea44960faccfce730ecb872f7b51744060
rishie <rishieruween199611@gmail.com>
2024-07-18 07:53:09 +0530

    Arr practice

--- src/Arrays/StringRev.java
+++ src/Arrays/StringRev.java
@@ -1,4 +1,24 @@
 package Arrays;
 
 public class StringRev {
+    public static void main(String[] args) {
+        String original = "Hello";
+        System.out.println("Original String is : " + original);
+        System.out.println("Reversed String is : " + stringReverse(original));
+
+    }
+
+    public static String stringReverse(String str) {
+        char[] chars = str.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
 }
